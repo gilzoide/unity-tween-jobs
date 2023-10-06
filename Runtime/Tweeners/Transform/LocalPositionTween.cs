0ad96a1a570739b662a650f56f8df28f814f8488
gilzoide <gilzoide@gmail.com>
2023-10-06 18:04:19 -0300

    Add LocalPositionTween

---
+++ Runtime/Tweeners/Transform/LocalPositionTween.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+namespace Gilzoide.TweenJobs.Tweeners
+{
+    [Serializable]
+    public class LocalPositionTweener : AVector3Tweener
+    {
+        public Transform Target;
+
+        public override Vector3 Value
+        {
+            get => Target.localPosition;
+            set => Target.localPosition = value;
+        }
+    }
+
+    public class LocalPositionTween : ATweenComponent<LocalPositionTweener>
+    {
+    }
+}
