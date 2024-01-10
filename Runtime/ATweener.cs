e7866edaca8860a5cdc4ff4c07158c960bdfa707
gilzoide <gilzoide@gmail.com>
2024-01-10 00:12:34 -0300

    Add some code documentation to ATweener

--- Runtime/ATweener.cs
+++ Runtime/ATweener.cs
@@ -16,6 +16,39 @@ namespace Gilzoide.TweenJobs
     {
         public static readonly TValueMath ValueMath;
 
+        [Tooltip("Initial tween value. If IsRelative is true, an initial reference value is added to this value before used.")]
+        [SerializeField] protected T _from;
+
+        [Tooltip("Final tween value. If IsRelative is true, an initial reference value is added to this value before used.")]
+        [SerializeField] protected T _to;
+
+        [Tooltip("Whether To and From values are absolute or relative to an initial reference value.")]
+        [SerializeField] protected bool _isRelative;
+
+        [Tooltip("Tween duration, in seconds. Zero and negative values make the tween end in the same frame as it starts.")]
+        [SerializeField, Min(0)] protected float _duration = 1;
+
+        [Tooltip("Tween speed multiplier. Negative values make the tween run backwards.")]
+        [SerializeField] protected float _speed = 1;
+
+        [Tooltip("If true, tweener will use Time.unscaledDeltaTime. Otherwise, use Time.deltaTime.")]
+        [SerializeField] protected bool _useUnscaledDeltaTime = false;
+
+        [Tooltip("Easing function used to interpolate values between To and From.")]
+        [SerializeField] protected Easings.Functions _easingFunction;
+
+        [Tooltip("How many times the tween will loop before completing. Set to -1 to loop forever.")]
+        [SerializeField, Min(-1)] protected int _loopCount;
+
+        [Tooltip("How the values will behave when looping, whether it should restart from the To value or ping-pong between To and From.")]
+        [SerializeField] protected LoopType _loopType;
+
+        #region Properties
+
+        /// <summary>
+        /// Initial tween value.
+        /// If <see cref="IsRelative"/> is true, <see cref="ReferenceValue"/> is added to this value before used.
+        /// </summary>
         public T From
         {
             get => _from;
@@ -28,6 +61,11 @@ namespace Gilzoide.TweenJobs
                 }
             }
         }
+
+        /// <summary>
+        /// Final tween value.
+        /// If <see cref="IsRelative"/> is true, <see cref="ReferenceValue"/> is added to this value before used.
+        /// </summary>
         public T To
         {
             get => _to;
@@ -40,6 +78,11 @@ namespace Gilzoide.TweenJobs
                 }
             }
         }
+
+        /// <summary>
+        /// Reference value used if <see cref="IsRelative"/> is true.
+        /// This value is automatically set with <see cref="Value"/> when first accessed.
+        /// </summary>
         public T ReferenceValue
         {
             get
@@ -62,6 +105,10 @@ namespace Gilzoide.TweenJobs
                 }
             }
         }
+
+        /// <summary>
+        /// Whether <see cref="To"/> and <see cref="From"/> values are absolute or relative to <see cref="ReferenceValue"/>.
+        /// </summary>
         public bool IsRelative
         {
             get => _isRelative;
@@ -74,6 +121,11 @@ namespace Gilzoide.TweenJobs
                 }
             }
         }
+
+        /// <summary>
+        /// Tween duration, in seconds.
+        /// Zero and negative values make the tween end in the same frame as it starts.
+        /// </summary>
         public float Duration
         {
             get => _duration;
@@ -86,6 +138,11 @@ namespace Gilzoide.TweenJobs
                 }
             }
         }
+
+        /// <summary>
+        /// Tween speed multiplier.
+        /// Negative values make the tween run backwards.
+        /// </summary>
         public float Speed
         {
             get => _speed;
@@ -98,6 +155,11 @@ namespace Gilzoide.TweenJobs
                 }
             }
         }
+
+        /// <summary>
+        /// Current tween time.
+        /// Set this to manually advance or rewind a tween.
+        /// </summary>
         public float Time
         {
             get => _time ?? this.GetJobData().Time;
@@ -110,6 +172,10 @@ namespace Gilzoide.TweenJobs
                 }
             }
         }
+
+        /// <summary>
+        /// Whether tweener will use Unity's unscaled delta time.
+        /// </summary>
         public bool UseUnscaledDeltaTime
         {
             get => _useUnscaledDeltaTime;
@@ -122,6 +188,10 @@ namespace Gilzoide.TweenJobs
                 }
             }
         }
+
+        /// <summary>
+        /// Easing function used to interpolate values between <see cref="To"/> and <see cref="From"/>.
+        /// </summary>
         public Easings.Functions EasingFunction
         {
             get => _easingFunction;
@@ -134,6 +204,11 @@ namespace Gilzoide.TweenJobs
                 }
             }
         }
+
+        /// <summary>
+        /// How many times this tween will loop before completing.
+        /// Set to -1 to loop forever.
+        /// </summary>
         public int LoopCount
         {
             get => _loopCount;
@@ -146,6 +221,10 @@ namespace Gilzoide.TweenJobs
                 }
             }
         }
+
+        /// <summary>
+        /// How the values will behave when looping, whether it should restart from the To value or ping-pong between To and From.
+        /// </summary>
         public LoopType LoopType
         {
             get => _loopType;
@@ -158,24 +237,30 @@ namespace Gilzoide.TweenJobs
                 }
             }
         }
-        public event Action OnComplete;
 
-        [SerializeField] protected T _from;
-        [SerializeField] protected T _to;
-        [SerializeField] protected bool _isRelative;
-        [SerializeField, Min(0)] protected float _duration = 1;
-        [SerializeField] protected float _speed = 1;
-        [SerializeField] protected bool _useUnscaledDeltaTime = false;
-        [SerializeField] protected Easings.Functions _easingFunction;
-        [SerializeField, Min(-1)] protected int _loopCount;
-        [SerializeField] protected LoopType _loopType;
+        #endregion
+
+        public event Action OnComplete;
 
         private bool _isDirty;
         protected T? _referenceValue;
         protected float? _time;
 
+        /// <summary>
+        /// Value being interpolated by the tweener.
+        /// Implement this in subclasses to define how to get the current value and what happens when the value changes.
+        /// </summary>
         public abstract T Value { get; set; }
 
+        /// <returns>
+        /// Whether tweener is playing or not.
+        /// </returns>
+        public bool IsPlaying => this.IsRegisteredInManager();
+
+        /// <summary>
+        /// Returns the initial internal job data.
+        /// You should never call this yourself.
+        /// </summary>
         public TweenJob<T, TValueMath> InitialJobData
         {
             get
@@ -198,14 +283,26 @@ namespace Gilzoide.TweenJobs
             }
         }
 
-        public bool IsPlaying => this.IsRegisteredInManager();
-
+        /// <summary>
+        /// Start playing the tween from the beginning.
+        /// </summary>
+        /// <remarks>
+        /// This method resets <see cref="Time"/> to 0.
+        /// Use <see cref="Unpause"/> instead if you don't want <see cref="Time"/> to change.
+        /// </remarks>
         public void Play()
         {
             _time = null;
             Unpause();
         }
 
+        /// <summary>
+        /// Start playing the tween forward from the beginning.
+        /// </summary>
+        /// <remarks>
+        /// If the current <see cref="Speed"/> is negative, it is reversed to make sure the tween plays forward.
+        /// Use <see cref="Play"/> instead if you don't want <see cref="Speed"/> to change.
+        /// </remarks>
         public void PlayForward()
         {
             if (Speed < 0)
@@ -215,6 +312,13 @@ namespace Gilzoide.TweenJobs
             Play();
         }
 
+        /// <summary>
+        /// Start playing the tween backward from the beginning.
+        /// </summary>
+        /// <remarks>
+        /// If the current <see cref="Speed"/> is positive, it is reversed to make sure the tween plays backwards.
+        /// Use <see cref="Play"/> instead if you don't want <see cref="Speed"/> to change.
+        /// </remarks>
         public void PlayBackward()
         {
             if (Speed > 0)
@@ -224,6 +328,9 @@ namespace Gilzoide.TweenJobs
             Play();
         }
 
+        /// <summary>
+        /// Pause the tween, if it was playing.
+        /// </summary>
         public void Pause()
         {
             if (IsPlaying)
@@ -233,6 +340,9 @@ namespace Gilzoide.TweenJobs
             }
         }
 
+        /// <summary>
+        /// Unpause the tween without changing the current <see cref="Time"/>.
+        /// </summary>
         public void Unpause()
         {
             if (Duration > 0)
@@ -245,6 +355,9 @@ namespace Gilzoide.TweenJobs
             }
         }
 
+        /// <summary>
+        /// Rewind the tween, setting <see cref="Value"/> to its first value.
+        /// </summary>
         public void Rewind()
         {
             _time = null;
@@ -257,6 +370,9 @@ namespace Gilzoide.TweenJobs
             Value = firstValue;
         }
 
+        /// <summary>
+        /// Complete the tween, setting <see cref="Value"/> to the final value.
+        /// </summary>
         public void Complete()
         {
             _time = null;
@@ -269,6 +385,10 @@ namespace Gilzoide.TweenJobs
             Value = finalValue;
         }
 
+        /// <summary>
+        /// Synchronizes internal job data.
+        /// You should never call this yourself.
+        /// </summary>
         public void SyncJobData(ref TweenJob<T, TValueMath> jobData)
         {
             Value = jobData.Value;
